2d252cfb20bfcadd37c6aa0fc8f2d56ba6004ce8
kalichavasalo <119868500+kalichavasalo@users.noreply.github.com>
2023-01-17 09:26:04 +0400

    create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log("Hello World") ; 
+let FirstName = "Salome";
+let LastName ="Kalichava";
+let age = 16;
+let birthplace = "Poti"
+let birthdate = "10th Feb"
+let isFemale = true;
+
+let Fullname = (`${FirstName} ${LastName}`);
+console.log(Fullname);
+
+let info = (`${FirstName} ${LastName} ${age}`); 
+console.log(info);
+
+let birthday = (birthplace + " " + birthdate);
+console.log (birthday);
+
+console.log(typeof isFemale);
+console.log(typeof age);
+
+let A = 13;
+let B = 5;
+
+let Sum = A + B;
+console.log(Sum);
+
+let sub = A - B;
+console.log (sub);
+
+let div = A / B;
+console.log(div);
+
+let multi = A * B;
+console.log (multi);
+
+let balance = A % B ;
+console.log(balance) ;
+
+let x = 7;
+let y = 5;
+let area = x * y;
+console.log(area);
+
+let R = 7;
+const PI = 3.14;
+
+let areacircle = PI * R * R ;
+console.log(areacircle);
